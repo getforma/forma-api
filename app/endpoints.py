37761d705418ecad69e89b174c5620350f9e7f91
Gerardo Mathus <gerardo@nextia.mx>
2024-10-01 18:21:06 -0400

    setup endpoints

---
+++ app/endpoints.py
@@ -0,0 +1,27 @@
+#import boto3
+from flask import request, jsonify
+from app.auth import auth_required
+from app.config import Config
+
+#dynamodb = boto3.resource('dynamodb')
+#table = dynamodb.Table(Config.DYNAMODB_TABLE)
+
+def register_endpoints(app):
+    @app.route('/', methods=['GET'])
+    def index():
+        return "hey"
+
+    @app.route('/sessions', methods=['POST'])
+    @auth_required
+    def create_new_session():
+        return jsonify({"message": "Running session started"}), 201
+
+    @app.route('/sessions/:id/track', methods=['POST'])
+    @auth_required
+    def track_session_data():
+        return jsonify({"message": "Tracked data"}), 200
+
+    @app.route('/sessions/:id/end', methods=['POST'])
+    @auth_required
+    def end_session():
+        return jsonify({"message": "Session ended"}), 200
