ed8cadaa20f5097b1bda4ef4c232ed71f99ff3a4
Gerardo Mathus <gerardo@nextia.mx>
2024-10-03 13:44:45 -0400

    clean endpoints script

--- app/endpoints.py
+++ app/endpoints.py
@@ -1,10 +1,6 @@
-#import boto3
 from flask import request, jsonify
 from app.auth import auth_required
-from app.config import Config
-from uuid import uuid4
-#dynamodb = boto3.resource('dynamodb')
-#table = dynamodb.Table(Config.DYNAMODB_TABLE)
+from app.entities.running_session import RunningSession
 
 def register_endpoints(app):
     @app.route('/', methods=['GET'])
@@ -14,15 +10,15 @@ def register_endpoints(app):
     @app.route('/sessions', methods=['POST'])
     @auth_required
     def create_new_session():
-        session_id = str(uuid4())
-        return jsonify({"message": f"Running session [{session_id}] started."}), 201
+        data = request.json
+        run = RunningSession(
+            device_id=data['device_id'],
+            device_position=data['device_position'],
+            user_name=data['user_name']
+        )
+        return jsonify(run.to_dict()), 201
 
     @app.route('/sessions/:id/track', methods=['POST'])
     @auth_required
     def track_session_data():
         return jsonify({"message": "Tracked data"}), 200
-
-    @app.route('/sessions/:id/end', methods=['POST'])
-    @auth_required
-    def end_session():
-        return jsonify({"message": "Session ended"}), 200
