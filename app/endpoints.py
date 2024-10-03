a30cc0d5fa7e5cf07082284ecbbe297b1ecdc5cf
Gerardo Mathus <gerardo@nextia.mx>
2024-10-03 10:53:07 -0400

    almost there

--- app/endpoints.py
+++ app/endpoints.py
@@ -9,7 +9,7 @@ from uuid import uuid4
 def register_endpoints(app):
     @app.route('/', methods=['GET'])
     def index():
-        return "hey, dog 3"
+        return "hey, dog 4"
 
     @app.route('/sessions', methods=['POST'])
     @auth_required
