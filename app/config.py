aef436fc5ccc014c58aa1b6d560721acac274280
Gerardo Mathus <gerardo@nextia.mx>
2024-10-03 13:44:53 -0400

    add second table

--- app/config.py
+++ app/config.py
@@ -2,6 +2,7 @@ import os
 
 class Config:
     DEBUG = os.getenv('FLASK_DEBUG', False)
-    DYNAMODB_TABLE = os.getenv('DYNAMODB_TABLE', 'forma-running-sessions')
+    RUNNING_SESSIONS_TABLE = os.getenv('RUNNING_SESSIONS_TABLE', 'forma-running-sessions')
+    RUNNING_SESSIONS_DATA_TABLE = os.getenv('RUNNING_SESSIONS_DATA_TABLE', 'forma-running-sessions-data')
     BASIC_AUTH_USERNAME = os.getenv('BASIC_AUTH_USERNAME', 'admin')
     BASIC_AUTH_PASSWORD = os.getenv('BASIC_AUTH_PASSWORD', 'password')
