178c1679acf9fccff8c569faffdcdde889909cdb
Gerardo Mathus <gerardo@nextia.mx>
2024-10-03 13:55:25 -0400

    no dynamo

--- app/entities/base_entity.py
+++ app/entities/base_entity.py
@@ -1,15 +1,18 @@
-import boto3
+#import boto3
 from uuid import uuid4
 from datetime  import datetime, timezone
-dynamodb = boto3.resource('dynamodb')
+#dynamodb = boto3.resource('dynamodb')
 
 class BaseEntity:
     def __init__(self, table):
         self.id = str(uuid4())
         self.created_at = datetime.now(timezone.utc).strftime('%Y-%m-%d %H:%M:%S.%f')
-        self.table = dynamodb.Table(table)
+        #self.table = dynamodb.Table(table)
+        self.table = "./tmp/" + table + ".txt"
     
     def save(self):
-        self.table.put_item(
-            Item=self.to_dict()
-        )
+        with open(self.table, 'a') as f:
+            f.write(self.to_dict())
+        # self.table.put_item(
+        #     Item=self.to_dict()
+        # )
