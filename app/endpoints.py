3cb04bdbfa5cb6f54bc8cbb22755003a8a91f685
Gerardo Mathus <gerardo@nextia.mx>
2024-10-24 10:38:29 -0400

    log

--- app/endpoints.py
+++ app/endpoints.py
@@ -25,6 +25,7 @@ def register_endpoints(app):
     def track_session_data(id):
         data = request.json
         for point in data:
+            print("===============> point", point)
             RunningSessionData(
                 running_session_id=id,
                 time=point['time'],
