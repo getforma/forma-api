2dbc9d998a5255245a9f4e4ad0f4922f09548be6
carzgil <cars.gilliland@gmail.com>
2024-10-24 13:56:23 -0400

    adding test cases for endpoints

---
+++ tests/test_endpoints.py
@@ -0,0 +1,27 @@
+import unittest
+import numpy as np
+import pandas as pd
+
+
+from app.utils.running_metrics import (
+    calculate_cadence,
+    calculate_distance,
+    calculate_speed,
+    calculate_vertical_oscillation,
+    calculate_stride_length,
+    calculate_ground_contact_time,
+    calculate_pace,
+    final_clean_data
+)
+
+class test_endpoints(unittest.TestCase):
+    def setUp(self):
+        """
+        importing sample data
+        """
+        self.sample_data = pd.read_csv('tests/running_mock_data.csv')
+        self.sample_data['time'] = pd.to_datetime(self.sample_data['time'], format="%Y-%m-%dT%H:%M:%S.%f")
+        self.sample_data, self.axis = final_clean_data(self.sample_data)
+
+    
+        
