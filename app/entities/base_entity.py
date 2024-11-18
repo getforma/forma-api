e4856e4c4f6dd68fc760dce507b274cbb0a4bd71
Gerardo Mathus <gerardo@nextia.mx>
2024-11-17 23:27:15 -0500

    use repository pattern

--- app/entities/base_entity.py
+++ app/entities/base_entity.py
@@ -1,15 +1,12 @@
-import boto3
 from uuid import uuid4
-from datetime  import datetime, timezone
-dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+from datetime import datetime, timezone
+from app.repositories.repository_factory import RepositoryFactory
 
 class BaseEntity:
     def __init__(self, table):
         self.id = str(uuid4())
         self.created_at = datetime.now(timezone.utc).strftime('%Y-%m-%d %H:%M:%S.%f')
-        self.table = dynamodb.Table(table)
+        self.repository = RepositoryFactory.get_repository(table)
     
     def save(self):
-        self.table.put_item(
-            Item=self.to_dict()
-        )
+        self.repository.put_item(self.to_dict())
